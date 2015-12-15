8644797c378dccfa61ab6498c84336306b081534
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-12-15 14:16:58 -0800

    added server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// Set the view engine && public folder
+app.set('view engine', 'html');
+app.set('views', __dirname);
+app.use(express.static(__dirname + '/public'));
+
+// Register root route
+app.get('/pixelArt', function(req, res) {
+  res.sendFile(__dirname + '/pixelArt/views/PixelArt.html');
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Server up and listening on', port);
+});
