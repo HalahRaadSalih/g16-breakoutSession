559621b2c3a279c431d8626f5692eebd3d71bd7b
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-12-15 15:52:49 -0800

    added route to whack a mole and inxed

--- PixelArt/controllers/router.js
+++ PixelArt/controllers/router.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 
 router.get('/', function(req, res) {
-	console.log(__dirname);
-  res.render('PixelArt');
+  	res.render('PixelArt');
+});
+
+router.get('/Mole', function(req, res) {
+  	res.render('index');
+});
+
+router.get('/Mole/whack-a-mole', function(req, res) {
+  	res.render('whack-a-mole');
 });
 
 module.exports = router;
